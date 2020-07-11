bc6ed7883f5e4886e081569db8b37922c3289c21
Kaan <kaangokdemir01@gmail.com>
2020-07-11 22:06:34 +0300

    Function added to type prop's type

--- docs/pages/components/taginput/api/taginput.js
+++ docs/pages/components/taginput/api/taginput.js
@@ -32,7 +32,7 @@ export default [
             {
                 name: '<code>type</code>',
                 description: 'Type (color) of the tags, optional',
-                type: 'String',
+                type: 'String, Function',
                 values: `<code>is-white</code>, <code>is-black</code>, <code>is-light</code>,
                     <code>is-dark</code>, <code>is-primary</code>, <code>is-info</code>, <code>is-success</code>,
                     <code>is-warning</code>, <code>is-danger</code>,
